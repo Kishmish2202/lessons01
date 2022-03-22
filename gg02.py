25f82e3d0946e3cb9337b45813b63cff0f7bd250
Tartar <101659868+YuliaTar@users.noreply.github.com>
2022-03-22 20:00:23 +0300

    Add files via upload

---
+++ gg02.py
@@ -0,0 +1,13 @@
+
+def plus (a,b):
+    gg = a + b
+    summa = 0
+    for plus01 in gg:
+        summa += plus01
+        
+        
+    return summa
+
+print (plus([1,2,3],[4,5,6]))
+print (plus([0],[0]))
+print (plus([-1,-2],[-3,-4]))
